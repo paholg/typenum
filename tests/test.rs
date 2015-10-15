d5a768334ae2901f035c07aed03a9b53d6658200
Paho Lurie-Gregg <paho@paholg.com>
2015-10-15 08:41:38 -0700

    More test mucking

--- tests/test.rs
+++ tests/test.rs
@@ -7,72 +7,98 @@ use std::path::Path;
 
 use typenum::build::{gen_uint, gen_int};
 
-/// Ensures that `<A as Op<B>>::Output` is the same type as `Result` where
-/// `A` is the type-level equivalent to 'a',
-/// `B` is the type-level equivalent to 'b',
-/// `Op` is trait named in 'op', which has associated type `Output`, and
-/// `Result` is the type-level equivalent to 'result'
 fn uint_binary_test(a: u64, op: &str, b: u64, result: u64) -> String {
     format!("
 {{
-    type A = {};
-    type B = {};
-    type Result = {};
+    type A = {a};
+    type B = {b};
+    type {result_name} = {result};
 
-    type Computed = <<A as {}<B>>::Output as Same<Result>>::Output;
-    assert_eq!(<Computed as Unsigned>::to_u64(), <Result as Unsigned>::to_u64());
+    type {computed_name} = <<A as {op}<B>>::Output as Same<{result_name}>>::Output;
+    assert_eq!(<{computed_name} as Unsigned>::to_u64(), <{result_name} as Unsigned>::to_u64());
 }}
-", gen_uint(a), gen_uint(b), gen_uint(result), op)
+",
+            a = gen_uint(a),
+            b=gen_uint(b),
+            result_name=format!("U{}", result),
+            result=gen_uint(result),
+            computed_name=format!("U{}_{}_U{}", a, op, b),
+            op=op)
 }
 
-/// Ensures that `<A as Op>::Output` is the same type as `Result` where
-/// `A` is the type-level equivalent to 'a',
-/// `Op` is trait named in 'op', which has associated type `Output`, and
-/// `Result` is the type-level equivalent to 'result'
 fn uint_unary_test(op: &str, a: u64, result: u64) -> String {
     format!("
 {{
-    type A = {};
-    type Result = {};
+    type A = {a};
+    type {result_name} = {result};
 
-    type Computed = <<A as {}>::Output as Same<Result>>::Output;
-    assert_eq!(<Computed as Unsigned>::to_u64(), <Result as Unsigned>::to_u64());
+    type {computed_name} = <<A as {op}>::Output as Same<{result_name}>>::Output;
+    assert_eq!(<{computed_name} as Unsigned>::to_u64(), <{result_name} as Unsigned>::to_u64());
 }}
-", gen_uint(a), gen_uint(result), op)
+",
+            a = gen_uint(a),
+            result_name=format!("U{}", result),
+            result=gen_uint(result),
+            computed_name=format!("U{}_{}", a, op),
+            op=op)
 }
 
-/// Ensures that `<A as Op<B>>::Output` is the same type as `Result` where
-/// `A` is the type-level equivalent to 'a',
-/// `B` is the type-level equivalent to 'b',
-/// `Op` is trait named in 'op', which has associated type `Output`, and
-/// `Result` is the type-level equivalent to 'result'
 fn int_binary_test(a: i64, op: &str, b: i64, result: i64) -> String {
+    let signa = if a > 0 { 'P' } else if a < 0 { 'N' } else { '_' };
+    let signb = if b > 0 { 'P' } else if b < 0 { 'N' } else { '_' };
+    let signr = if result > 0 { 'P' } else if result < 0 { 'N' } else { '_' };
     format!("
 {{
-    type A = {};
-    type B = {};
-    type Result = {};
+    type A = {a};
+    type B = {b};
+    type {result_name} = {result};
 
-    type Computed = <<A as {}<B>>::Output as Same<Result>>::Output;
-    assert_eq!(<Computed as Integer>::to_i64(), <Result as Integer>::to_i64());
+    type {computed_name} = <<A as {op}<B>>::Output as Same<{result_name}>>::Output;
+    assert_eq!(<{computed_name} as Integer>::to_i64(), <{result_name} as Integer>::to_i64());
 }}
-", gen_int(a), gen_int(b), gen_int(result), op)
+",
+            a = gen_int(a),
+            b=gen_int(b),
+            result_name=format!("{}{}", signr, result.abs()),
+            result=gen_int(result),
+            computed_name=format!("{}{}_{}_{}{}", signa, a.abs(), op, signb, b.abs()),
+            op=op)
 }
 
-/// Ensures that `<A as Op>::Output` is the same type as `Result` where
-/// `A` is the type-level equivalent to 'a',
-/// `Op` is trait named in 'op', which has associated type `Output`, and
-/// `Result` is the type-level equivalent to 'result'
 fn int_unary_test(op: &str, a: i64, result: i64) -> String {
+    let signa = if a > 0 { 'P' } else if a < 0 { 'N' } else { '_' };
+    let signr = if result > 0 { 'P' } else if result < 0 { 'N' } else { '_' };
     format!("
 {{
-    type A = {};
-    type Result = {};
+    type A = {a};
+    type {result_name} = {result};
 
-    type Computed = <<A as {}>::Output as Same<Result>>::Output;
-    assert_eq!(<Computed as Integer>::to_i64(), <Result as Integer>::to_i64());
+    type {computed_name} = <<A as {op}>::Output as Same<{result_name}>>::Output;
+    assert_eq!(<{computed_name} as Integer>::to_i64(), <{result_name} as Integer>::to_i64());
 }}
-", gen_int(a), gen_int(result), op)
+",
+            a = gen_int(a),
+            result_name=format!("{}{}", signr, result.abs()),
+            result=gen_int(result),
+            computed_name=format!("{}{}_{}", signa, a.abs(), op),
+            op=op)
+}
+
+fn uint_cmp_test(a: u64, b: u64) -> String {
+    format!("
+{{
+    type A = {a};
+    type B = {b};
+    type Result = {result:?};
+
+    type {computed_name} = <A as Cmp<B>>::Output;
+    assert_eq!(<{computed_name} as Ord>::to_ordering(), Ordering::{result:?});
+}}
+",
+            a = gen_uint(a),
+            b = gen_uint(b),
+            result = a.cmp(&b),
+            computed_name = format!("U{}_Cmp_U{}", a, b))
 }
 
 /// Runs the test strings. Expects output from the test functions in this module.
@@ -104,10 +130,12 @@ git = \"file:{}\"
         let mut f = File::create(&main).unwrap();
         f.write(format!("
 #![allow(unused_imports)]
+#![allow(non_camel_case_types)]
 extern crate typenum;
 
 use std::ops::{{BitAnd, BitOr, BitXor, Shl, Shr, Neg, Add, Sub, Mul, Div, Rem}};
-use typenum::{{NonZero, Same, Pow, Ord, Cmp, SizeOf}};
+use std::cmp::Ordering;
+use typenum::{{NonZero, Same, Pow, Ord, Cmp, SizeOf, Greater, Less, Equal}};
 use typenum::bit::{{Bit, B0, B1}};
 use typenum::uint::{{Unsigned, UInt, UTerm}};
 use typenum::int::{{Integer, NInt, PInt, Z0}};
@@ -119,7 +147,7 @@ fn main() {{
 ", tests.join("\n")).as_bytes()).unwrap();
     }
 
-    Command::new("cargo").arg("clean").current_dir(&test_dir).output().unwrap();
+    Command::new("cargo").arg("update").current_dir(&test_dir).output().unwrap();
     let test_out = Command::new("cargo").arg("run").current_dir(&test_dir).output().unwrap();
     if !test_out.status.success() {
         let stdout = ::std::str::from_utf8(&test_out.stdout).unwrap();
@@ -128,27 +156,34 @@ fn main() {{
     }
 }
 
-// #[test]
-// fn sample_runtime_tests() {
-//     let tests = vec![
-//         uint_binary_test(7, "Div", 2, 3),
-//         uint_unary_test("SizeOf", 17, 5),
-
-//         int_binary_test(7, "Add", 2, 9),
-//         int_unary_test("Neg", 3, -3)
-//             ];
-//     run_tests(tests);
-// }
-
 #[test]
-fn test_ints() {
-    let inputs = vec![(1, 2), (3, 4)];
-    let tests = inputs.iter().map(|&(a, b)| int_binary_test(a, "Add", b, a + b))
-        .chain(inputs.iter().map(|&(a, b)| int_binary_test(a, "Sub", b, a - b)))
-        .chain(inputs.iter().map(|&(a, b)| int_binary_test(a, "Mul", b, a * b)))
-        .chain(inputs.iter().filter(|&&(_, b)| b != 0).map(|&(a, b)| int_binary_test(a, "Div", b, a / b)))
+fn test_all() {
+    //let uints = (0..9).map(|a| );
+    let uints = vec![(0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (3, 4)];
+    let ints = vec![(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1), (1, 2), (3, 4)];
+
+    // uint operators: BitAnd, BitOr, BitXor, Shl, Shr, Add, Sub, Mul, Div, Pow, Cmp, SizeOf
+    // fixme: add cmp and sizeof
+    let uint_tests = uints.iter().map(|&(a, b)| uint_binary_test(a, "BitAnd", b, a & b))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "BitOr", b, a | b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "BitXor", b, a ^ b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "Shl", b, a << b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "Shr", b, a >> b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "Add", b, a + b)))
+        .chain(uints.iter().filter(|&&(a, b)| a >= b).map(|&(a, b)| uint_binary_test(a, "Sub", b, a - b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "Mul", b, a * b)))
+        .chain(uints.iter().filter(|&&(_, b)| b != 0).map(|&(a, b)| uint_binary_test(a, "Div", b, a / b)))
+        .chain(uints.iter().map(|&(a, b)| uint_binary_test(a, "Pow", b, a.pow(b as u32))))
+        .chain(uints.iter().map(|&(a, b)| uint_cmp_test(a, b)))
         ;
-    run_tests(tests.collect())
+    // run_tests(uint_tests.collect());
+
+    // int operators: Neg, Add, Sub, Mul, Div, Pow, Cmp
+    let int_tests = ints.iter().map(|&(a, _)| int_unary_test("Neg", a, -a))
+        .chain(ints.iter().map(|&(a, b)| int_binary_test(a, "Add", b, a + b)))
+        .chain(ints.iter().map(|&(a, b)| int_binary_test(a, "Sub", b, a - b)))
+        .chain(ints.iter().map(|&(a, b)| int_binary_test(a, "Mul", b, a * b)))
+        .chain(ints.iter().filter(|&&(_, b)| b != 0).map(|&(a, b)| int_binary_test(a, "Div", b, a / b)))
+        ;
+    // run_tests(int_tests.collect());
 }
-
-
