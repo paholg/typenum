e8051dafde1cf374abdb9dfa56b3535ccb4f671c
Paho Lurie-Gregg <paho@paholg.com>
2015-12-29 09:57:04 -0800

    Removed aliases for mul and div by constants

--- src/lib.rs
+++ src/lib.rs
@@ -203,26 +203,6 @@ pub type Add1<A> = <A as Add<bit::B1>>::Output;
 /// Alias to make it easy to subtract 1: `Sub1<A> = <A as Sub<B1>>::Output`
 pub type Sub1<A> = <A as Sub<bit::B1>>::Output;
 
-/// Alias to make it easy to multiply by `U2`: `MulU2<A> = <A as Mul<U2>>::Output`
-pub type MulU2<A> = <A as Mul<consts::U2>>::Output;
-/// Alias to make it easy to multiply by `U10`: `MulU10<A> = <A as Mul<U10>>::Output`
-pub type MulU10<A> = <A as Mul<consts::U10>>::Output;
-
-/// Alias to make it easy to multiply by `P2`: `MulP2<A> = <A as Mul<P2>>::Output`
-pub type MulP2<A> = <A as Mul<consts::P2>>::Output;
-/// Alias to make it easy to multiply by `P10`: `MulP10<A> = <A as Mul<P10>>::Output`
-pub type MulP10<A> = <A as Mul<consts::P10>>::Output;
-
-/// Alias to make it easy to divide by `U2`: `DivU2<A> = <A as Div<U2>>::Output`
-pub type DivU2<A> = <A as Div<consts::U2>>::Output;
-/// Alias to make it easy to divide by `U10`: `DivU10<A> = <A as Div<U10>>::Output`
-pub type DivU10<A> = <A as Div<consts::U10>>::Output;
-
-/// Alias to make it easy to divide by `P2`: `DivP2<A> = <A as Div<P2>>::Output`
-pub type DivP2<A> = <A as Div<consts::P2>>::Output;
-/// Alias to make it easy to divide by `P10`: `DivP10<A> = <A as Div<P10>>::Output`
-pub type DivP10<A> = <A as Div<consts::P10>>::Output;
-
 /// Alias to make it easy to square. `Square<A> = <A as Mul<A>>::Output`
 pub type Square<A> = <A as Mul>::Output;
 /// Alias to make it easy to square. `Cube<A> = <Square<A> as Mul<A>>::Output`
