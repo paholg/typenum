74d63c1eae7a40514192a1f16706e6fadef62b56
Paho Lurie-Gregg <paho@paholg.com>
2025-02-17 23:21:42 -0800

    Add constant for 3600 (#220)

--- generate/src/main.rs
+++ generate/src/main.rs
@@ -43,7 +43,10 @@ const HIGHEST: u64 = 1024;
 fn uints() -> impl Iterator<Item = u64> {
     let first2: u32 = (HIGHEST as f64).log(2.0).round() as u32 + 1;
     let first10: u32 = (HIGHEST as f64).log(10.0) as u32 + 1;
+
+    let other_constants = [3600];
     (0..(HIGHEST + 1))
+        .chain(other_constants)
         .chain((first2..64).map(|i| 2u64.pow(i)))
         .chain((first10..20).map(|i| 10u64.pow(i)))
 }
